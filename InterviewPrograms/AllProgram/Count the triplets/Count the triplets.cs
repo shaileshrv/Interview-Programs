c37358896ab50b6c4953d0894f91a3e9b8c4a407
Angel <Angel@DESKTOP-644D4F0>
2019-12-17 23:51:33 +0530

    Count the triplets

---
+++ InterviewPrograms/AllProgram/Count the triplets/Count the triplets.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrograms
+{
+    public static partial class Utility
+    {
+        public static void Count_the_triplets(int[] arr)
+        {
+            List<string> list = new List<string>();
+            for (int index = 0; index < arr.Length-1; index++)
+            {
+                for (int index_j = index + 1; index_j < arr.Length ; index_j++)
+                {
+                    if (arr.Contains(arr[index] + arr[index_j]))
+                    {
+                        list.Add("{" + arr[index] + "," + arr[index_j] + "}");
+                    }
+                }
+            }
+        }
+    }
+}
