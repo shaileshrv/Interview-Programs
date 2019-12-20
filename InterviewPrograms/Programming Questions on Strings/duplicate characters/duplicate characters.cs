bcc07d622231ca24a6405c82049e4616f73bf8b9
Shailesh Vishwakarma <v-shvi@microsoft.com>
2019-12-20 20:52:12 +0530

    update

---
+++ InterviewPrograms/Programming Questions on Strings/duplicate characters/duplicate characters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrograms
+{
+    public static partial class Utility
+    {
+        public static char[] duplicate_characters(string str)
+        {
+            var charCount = GetCharCount(str);
+            var duplicateChar = new List<char>();
+            foreach (var item in charCount)
+            {
+                if (item.Value > 1)
+                {
+                    duplicateChar.Add(item.Key);
+                }
+            }
+            return duplicateChar.ToArray();
+        }
+    }
+}
