dc998b0c215d548ecf10fc55c79ca466e7bb001f
Angel <Angel@DESKTOP-644D4F0>
2019-12-21 02:12:45 +0530

    update

---
+++ InterviewPrograms/Programming Questions on  Numbers and Arrays/Palindromic number/Palindromic number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrograms
+{
+   public static partial class Utility
+    {
+        public static int Reverse_number(int number)
+        {
+            int rev = 0;
+            while (number>0)
+            {
+                int lastDigit = number % 10;
+                rev =(rev* 10) + lastDigit;
+                number /= 10;
+            }
+            return rev;
+        }
+    
+        public static bool Palindromic_number(int number)
+        {
+            return number == Reverse_number(number);
+        }
+    }
+}
---
+++ InterviewPrograms/Recursion/sum of digits of a number/sum of digits of a number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrograms
+{
+  public static partial  class Utility
+    {
+        public static int sum_of_digits_of_a_number(int number)
+        {
+            if (number == 0)
+            {
+                return number;
+            }
+            int sum = 0;
+            sum += number%10 + sum_of_digits_of_a_number(number / 10);
+            return sum;
+        }
+    }
+}
