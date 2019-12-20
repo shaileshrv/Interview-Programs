61c7a0dd9d9c69e0083b3e1718a5ab8fa526515b
Shailesh Vishwakarma <v-shvi@microsoft.com>
2019-12-20 20:56:09 +0530

    add

---
+++ InterviewPrograms/Programming Questions on Strings/Reverse String/Reverse_String.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrograms
+{
+    public static partial class  Utility
+    {
+        public static string Reverse_String(string str)
+        {
+            string reverse = string.Empty;
+            for (int i = str.Length-1; i >=0 ; i--)
+            {
+                reverse += str[i];
+            }
+            return reverse;
+        }
+    }
+}
